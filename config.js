3b5b0883922d7b0d64c89480184e903b0e5e4605
RNSM SOSA <vlady1923s@gmail.com>
2025-09-19 15:55:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://Sosa:<db_password>@discordbot.nyjmsex.mongodb.net/?retryWrites=true&w=majority&appName=DiscordBot",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
